1be87e0af900894a969b1e3b4b61161af0c0271e
usmanatrulrr <usman@rulrr.com>
2026-09-03 13:28:56 +0500

    config: update frontend port to 3001 and backend port to 3002

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3001', 'http://localhost:3000'],
   });
 
   app.useGlobalPipes(
